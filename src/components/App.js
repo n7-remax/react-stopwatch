fbe1c43b405e8e25e209dc7153e9f27b547cb515
n7-remax <ditritusa@gmail.com>
2020-12-17 21:40:15 +0200

    created template for stopwatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [isRunning, setIsRunning] = useState(0);
   return (
     <div className="App">
+      <div className="time">
+        {hours} : {minutes} : {seconds}
+      </div>
+      <div className="control-buttons">
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
